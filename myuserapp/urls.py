29654b6296b710d57afdd005abee0e5f27dbcb7e
pruthesh2006-tech <pruthesh2006@gmail.com>
2026-06-20 14:29:53 +0530

    Complete django applications

--- myuserapp/urls.py
+++ myuserapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepageview),
-    path('home', views.homepageview),
+    path('home/', views.homepageview),
     path('about', views.aboutpageview),
     path('contact', views.contactpageview),
     path('shop', views.shoppage),
